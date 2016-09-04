149434e554ba9b9e9106f7fb019a21e1efb90f4a
raingloom <raingloom42@gmail.com>
2016-09-04 02:17:13 +0200

    MUCH improved build script

---
+++ build.sh
@@ -0,0 +1,78 @@
+#!/bin/zsh
+if [ "$EUID" -ne 0 ]
+then
+#not running as root, re-run with sudo
+	sudo $0 $@
+else
+###################
+#Main section
+#Should be root now
+###################
+
+#assign defaults
+here="$(dirname $0)"
+#get absolute path of this script
+if [ ${here[1]} != '/' ]
+then
+	here="${PWD}/${here}"
+fi
+work="${here}/work"
+container="${here}/container"
+repo="${here}/repo"
+
+snap() {
+	local rw=""
+	[[ $1 = '-w' ]] && rw="" || rw="-r"
+	btrfs subvolume snapshot $rw "$container" "${work}/snapshots/$(date +%s)"
+}
+
+run-once() {
+	local f="${work}/ran-${1}"
+	if [ -f "$f" ]
+	then
+		$@
+	else
+		touch "$f"
+		$@
+	fi
+}
+
+nspawn() {
+	sudo systemd-nspawn --directory "$container" --chdir="/root/repo" $@
+}
+
+xnspawn() {
+	nspawn $@
+}
+
+init() {
+	mkdir "$work"
+	mkdir "${work}/snapshots"
+	btrfs subvolume create "$container"
+	pacstrap -d "$container" base base-devel zsh grml-zsh-config --ignore linux 
+	cp -rvfT "${here}/copied" "${container}/root"
+	cp -rvT "$repo" "${container}/root/repo"
+}
+
+mkautologin() {
+	autologin="${container}/etc/systemd/system/getty@tty.service.d/override.conf"
+	mkdir -p "$(dirname $autologin)"
+	echo '
+	[Service]
+	ExecStart=
+	ExecStart=-/usr/bin/agetty --autologin username --noclear %I $TERM' > "$autologin"
+}
+
+cleanup() {
+	rm -r "$work"
+}
+
+buildiso() {
+	xnspawn "releng/fullbuild.sh"
+}
+
+###################################
+$@ #execute command line parameters
+###################################
+
+fi
